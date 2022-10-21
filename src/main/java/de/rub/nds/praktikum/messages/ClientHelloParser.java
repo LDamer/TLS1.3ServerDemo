78c3366ba8d631ee8f76e53cda2fd6f88bc156e2
LDamer <leon@damers.de>
2022-10-22 00:16:28 +0200

    alpha version. should work if extensionParsers are implemented

--- src/main/java/de/rub/nds/praktikum/messages/ClientHelloParser.java
+++ src/main/java/de/rub/nds/praktikum/messages/ClientHelloParser.java
@@ -1,10 +1,6 @@
 package de.rub.nds.praktikum.messages;
 
-import de.rub.nds.praktikum.constants.CipherSuite;
-import de.rub.nds.praktikum.constants.CompressionMethod;
-import de.rub.nds.praktikum.constants.ExtensionType;
-import de.rub.nds.praktikum.constants.FieldLength;
-import de.rub.nds.praktikum.constants.ProtocolVersion;
+import de.rub.nds.praktikum.constants.*;
 import de.rub.nds.praktikum.exception.ParserException;
 import de.rub.nds.praktikum.messages.extensions.Extension;
 import de.rub.nds.praktikum.messages.extensions.KeyShareExtension;
@@ -19,6 +15,9 @@ import de.rub.nds.praktikum.util.Util;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.ProtocolFamily;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -45,7 +44,79 @@ public class ClientHelloParser extends Parser<ClientHello> {
 
     @Override
     public ClientHello parse() {
-        throw new UnsupportedOperationException("Add code here");
+        //should work!
+
+        //parse handshakeType
+
+        /* In handshake layer, not here!
+        HandshakeMessageType handshake_type;
+        if(parseByteField() == HandshakeMessageType.CLIENT_HELLO.getValue()){
+            handshake_type = HandshakeMessageType.CLIENT_HELLO;
+        }else{
+            throw new ParserException("ClientHelloParser: this is not a ClientHello message[wrong mesage type byte]");
+        }
+
+        //parse length
+        int lenOfHandshake = parseIntField(FieldLength.HANDSHAKE_LENGTH);
+        */
+
+        //parse version
+        ProtocolVersion version_in_header = ProtocolVersion.convert(parseByteArrayField(FieldLength.VERSION));
+        //parse ClientRandom
+        byte[] clientRandom = parseByteArrayField(FieldLength.RANDOM);
+        //parse useless session ID. Deprecated in TLS 1.3
+        int sessionID_len = parseIntField(FieldLength.SESSION_ID_LENGTH);
+        byte[] sessionID_fake = parseByteArrayField(sessionID_len);
+        //parse CipherSuites
+        int cipherSuitesLen = parseIntField(FieldLength.CIPHER_SUITE_LENGTH);
+        ArrayList<CipherSuite> cipherList = new ArrayList<>();
+        for(int i = 0; i < cipherSuitesLen; i+=2){
+            cipherList.add(CipherSuite.convert(parseByteArrayField(2)));
+        }
+
+        //parse compression. No compression in TLS 1.3
+        int compressionMethodsLen = parseIntField(1);
+        int compressionMethod = parseIntField(1);
+        ArrayList<CompressionMethod> compressionList = new ArrayList<>();
+        compressionList.add(CompressionMethod.NULL);
+        if(compressionMethodsLen != 1 || compressionMethod != 0){
+            throw new ParserException("ClientHelloParser: compression must be a null byte!");
+        }
+
+        //parse ExtensionLen
+        int lenOfAllExtension = parseIntField(FieldLength.EXTENSIONS_LENGTH);
+        //parse extensions
+        ArrayList<Extension> extensionsList = new ArrayList<>();
+        int seenBytes = 0;
+        while(seenBytes < lenOfAllExtension){
+            int headerLenOfExtension = FieldLength.EXTENSION_LENGTH+FieldLength.EXTENSION_TYPE;
+            ExtensionType type = ExtensionType.convert(parseByteArrayField(FieldLength.EXTENSION_TYPE));
+            int lenOfThisExtension = parseIntField(FieldLength.EXTENSION_LENGTH);
+            setPointer(getPointer()-headerLenOfExtension);// set pointer back to start of extension
+            if(type == ExtensionType.KEY_SHARE){
+                KeyShareExtensionParser parser = new KeyShareExtensionParser(parseByteArrayField(lenOfThisExtension + headerLenOfExtension));
+                KeyShareExtension share = parser.parse();
+                extensionsList.add(share);
+            }else if(type == ExtensionType.SUPPORTED_VERSIONS){
+                SupportedVersionsExtensionParser parser = new SupportedVersionsExtensionParser(parseByteArrayField(lenOfThisExtension + headerLenOfExtension));
+                SupportedVersionsExtension sv = parser.parse();
+                extensionsList.add(sv);
+            }else if(type == ExtensionType.SUPPORTED_GROUPS){
+                SupportedGroupsExtensionParser parser = new SupportedGroupsExtensionParser(parseByteArrayField(lenOfThisExtension + headerLenOfExtension));
+                SupportedGroupsExtension e = parser.parse();
+                extensionsList.add(e);
+            }else if(type == ExtensionType.SUPPORTED_SIGNATURES){
+                SupportedSignaturesAlgorithmExtensionParser parser = new SupportedSignaturesAlgorithmExtensionParser(parseByteArrayField(lenOfThisExtension + headerLenOfExtension));
+                SupportedSignaturesAlgorithmExtension e = parser.parse();
+                extensionsList.add(e);
+            }else{
+                //just dismiss the bytes
+                parseArrayOrTillEnd(lenOfThisExtension + headerLenOfExtension);
+            }
+            seenBytes += lenOfThisExtension + headerLenOfExtension;// +2 for type +2 for len fields
+        }
+        ClientHello ch = new ClientHello(version_in_header,clientRandom,sessionID_fake, cipherList, compressionList, extensionsList);
+        return ch;
     }
 
 }
