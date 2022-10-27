37fd6c82c16bfc8f7136dca64cb04bb227e18a64
LDamer <leon@damers.de>
2022-10-27 16:00:13 +0200

    implemented

--- src/main/java/de/rub/nds/praktikum/messages/extensions/SupportedVersionsExtensionParser.java
+++ src/main/java/de/rub/nds/praktikum/messages/extensions/SupportedVersionsExtensionParser.java
@@ -1,8 +1,11 @@
 package de.rub.nds.praktikum.messages.extensions;
 
 import de.rub.nds.praktikum.constants.ProtocolVersion;
+import de.rub.nds.praktikum.constants.SignatureAndHashAlgorithm;
 import de.rub.nds.praktikum.exception.ParserException;
 import de.rub.nds.praktikum.messages.Parser;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,7 +26,18 @@ public class SupportedVersionsExtensionParser extends Parser<SupportedVersionsEx
 
     @Override
     public SupportedVersionsExtension parse() {
-        throw new UnsupportedOperationException("Add code here");
+        //throw new UnsupportedOperationException("Add code here");
+        int payloadLength = parseIntField(1);
+        ArrayList<ProtocolVersion> versionsList = new ArrayList<>();
+        for(int i = 0; i < payloadLength; i += 2){
+            byte[] version = parseByteArrayField(2);
+            ProtocolVersion v = ProtocolVersion.convert(version);
+            versionsList.add(v);
+        }
+        if(getBytesLeft() != 0){
+            throw new ParserException("bytesLeft != 0");
+        }
+        return new SupportedVersionsExtension(versionsList);
     }
 
 }
