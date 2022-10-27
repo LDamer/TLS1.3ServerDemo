59479dd95985b09285a968372272c983260e8f5c
LDamer <leon@damers.de>
2022-10-27 15:35:46 +0200

    implemented

--- src/main/java/de/rub/nds/praktikum/messages/extensions/SupportedSignaturesAlgorithmExtensionParser.java
+++ src/main/java/de/rub/nds/praktikum/messages/extensions/SupportedSignaturesAlgorithmExtensionParser.java
@@ -1,9 +1,12 @@
 package de.rub.nds.praktikum.messages.extensions;
 
 import de.rub.nds.praktikum.constants.FieldLength;
+import de.rub.nds.praktikum.constants.NamedGroup;
 import de.rub.nds.praktikum.constants.SignatureAndHashAlgorithm;
 import de.rub.nds.praktikum.exception.ParserException;
 import de.rub.nds.praktikum.messages.Parser;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,7 +26,19 @@ public class SupportedSignaturesAlgorithmExtensionParser extends Parser<Supporte
 
     @Override
     public SupportedSignaturesAlgorithmExtension parse() {
-        throw new UnsupportedOperationException("Add code here");
+        //throw new UnsupportedOperationException("Add code here");
+        int payloadLength = parseIntField(2);
+        ArrayList<SignatureAndHashAlgorithm> algorithms = new ArrayList<>();
+        for(int i = 0; i < payloadLength; i += 2){
+            byte[] namedGroup = parseByteArrayField(2);
+            SignatureAndHashAlgorithm g = SignatureAndHashAlgorithm.convert(namedGroup);
+            algorithms.add(g);
+        }
+        if(getBytesLeft() != 0){
+            throw new ParserException("bytesLeft != 0");
+        }
+        return new SupportedSignaturesAlgorithmExtension(algorithms);
+
     }
 
 }
