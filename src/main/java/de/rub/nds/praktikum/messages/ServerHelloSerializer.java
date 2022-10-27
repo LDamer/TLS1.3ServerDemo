4e29caff0433dde5ecf4a4fec3ea7418d766eddb
LDamer <leon@damers.de>
2022-10-27 17:49:42 +0200

    working

--- src/main/java/de/rub/nds/praktikum/messages/ServerHelloSerializer.java
+++ src/main/java/de/rub/nds/praktikum/messages/ServerHelloSerializer.java
@@ -1,5 +1,6 @@
 package de.rub.nds.praktikum.messages;
 
+import de.rub.nds.praktikum.constants.ExtensionType;
 import de.rub.nds.praktikum.exception.TlsException;
 import de.rub.nds.praktikum.messages.extensions.Extension;
 import de.rub.nds.praktikum.util.Util;
@@ -25,7 +26,40 @@ public class ServerHelloSerializer extends Serializer<ServerHello> {
 
     @Override
     protected void serializeBytes() {
-        throw new UnsupportedOperationException("Add code here");
-    }
+        //throw new UnsupportedOperationException("Add code here");
+        appendBytes(hello.getVersion().getValue());
+        appendBytes(hello.getRandom());
+        appendByte((byte)hello.getSessionId().length);
+        appendBytes(hello.getSessionId());
+        appendBytes(hello.getSelectedCiphersuite().getValue());
+        appendBytes(hello.getSelectedCompressionMethod().getValue());
 
+        int lenOfExtensions = 0;
+        for(Extension e : hello.getExtensionList()){
+            if(e.getType().equals(ExtensionType.KEY_SHARE)
+                || e.getType().equals(ExtensionType.SUPPORTED_VERSIONS)){
+                    lenOfExtensions += e.getSerializer().serialize().length;
+                    lenOfExtensions += 2 + 2;//type + len
+            }
+        }
+        appendInt(lenOfExtensions, 2);
+
+        for(Extension e : hello.getExtensionList()){
+            if(e.getType().equals(ExtensionType.SUPPORTED_VERSIONS)){
+                appendBytes(e.getType().getValue());
+                appendInt(e.getSerializer().serialize().length, 2);
+                appendBytes(e.getSerializer().serialize());
+                break;
+            }
+        }
+        for(Extension e : hello.getExtensionList()){
+            if(e.getType().equals(ExtensionType.KEY_SHARE)){
+                appendBytes(e.getType().getValue());
+                appendInt(e.getSerializer().serialize().length, 2);
+                appendBytes(e.getSerializer().serialize());
+                break;
+            }
+        }
+
+    }
 }
