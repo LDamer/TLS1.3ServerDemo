e6fa9b572b38a1916db882d983f89efb841d4182
LDamer <leon@damers.de>
2022-10-27 15:59:54 +0200

    implemented

--- src/main/java/de/rub/nds/praktikum/messages/extensions/SupportedVersionsExtensionSerializer.java
+++ src/main/java/de/rub/nds/praktikum/messages/extensions/SupportedVersionsExtensionSerializer.java
@@ -1,6 +1,10 @@
 package de.rub.nds.praktikum.messages.extensions;
 
+import de.rub.nds.praktikum.constants.ProtocolVersion;
+import de.rub.nds.praktikum.exception.ParserException;
+import de.rub.nds.praktikum.exception.SerializerException;
 import de.rub.nds.praktikum.messages.Serializer;
+import de.rub.nds.praktikum.util.Util;
 
 /**
  * A serializer class which transforms a supported versions into its byte
@@ -21,7 +25,22 @@ public class SupportedVersionsExtensionSerializer extends Serializer<SupportedVe
 
     @Override
     protected void serializeBytes() {
-        throw new UnsupportedOperationException("Add code here");
+        //throw new UnsupportedOperationException("Add code here");
+
+        //appendBytes(extension.getType().getValue());
+
+        //int lengthInHeader = extension.getSupportedVersions().size() * 2 + 1;
+        //int lengthInPayload = lengthInHeader - 1;
+        //appendInt((byte)lengthInHeader,2);
+        //int verifyPayLoadLength = 0;
+        for(ProtocolVersion p : extension.getSupportedVersions()){
+            appendInt(Util.convertToInt(p.getValue()),2);
+            //verifyPayLoadLength += 2;
+        }
+        //if(verifyPayLoadLength != lengthInPayload){
+        //    throw new SerializerException();
+        //}
+
     }
 
 }
