8ec62c9c8a988cb2c3f83b066bed4cb105cd6ff4
LDamer <leon@damers.de>
2022-10-27 15:32:50 +0200

    implemented

--- src/main/java/de/rub/nds/praktikum/messages/extensions/SupportedGroupsExtensionParser.java
+++ src/main/java/de/rub/nds/praktikum/messages/extensions/SupportedGroupsExtensionParser.java
@@ -4,6 +4,8 @@ import de.rub.nds.praktikum.constants.FieldLength;
 import de.rub.nds.praktikum.constants.NamedGroup;
 import de.rub.nds.praktikum.exception.ParserException;
 import de.rub.nds.praktikum.messages.Parser;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,7 +25,18 @@ public class SupportedGroupsExtensionParser extends Parser<SupportedGroupsExtens
 
     @Override
     public SupportedGroupsExtension parse() {
-        throw new UnsupportedOperationException("Add code here");
+        //throw new UnsupportedOperationException("Add code here");
+        int payloadLength = parseIntField(2);
+        ArrayList<NamedGroup> groups = new ArrayList<>();
+        for(int i = 0; i < payloadLength; i += 2){
+            byte[] namedGroup = parseByteArrayField(2);
+            NamedGroup g = NamedGroup.convert(namedGroup);
+            groups.add(g);
+        }
+        if(getBytesLeft() != 0){
+            throw new ParserException("bytesLeft != 0");
+        }
+        return new SupportedGroupsExtension(groups);
     }
 
 }
