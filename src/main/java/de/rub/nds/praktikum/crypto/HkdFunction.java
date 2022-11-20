0ead4412353c950e006663adfce96adad8ca21db
LDamer <leon@damers.de>
2022-11-20 17:37:56 +0100

    HKDF working

--- src/main/java/de/rub/nds/praktikum/crypto/HkdFunction.java
+++ src/main/java/de/rub/nds/praktikum/crypto/HkdFunction.java
@@ -4,12 +4,15 @@ import de.rub.nds.praktikum.exception.TlsException;
 import de.rub.nds.praktikum.util.Util;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import javax.crypto.Mac;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
 
 /**
  * HKDF-Function which is used in TLS 1.3 for the key derivation
@@ -56,6 +59,17 @@ public class HkdFunction {
      */
     public static final String SERVER_APPLICATION_TRAFFIC_SECRET = "s ap traffic";
 
+    private static byte[] hmacSHA256(byte[] salt, byte[] data){
+        SecretKey sk = new SecretKeySpec(salt, "HmacSHA256");
+        try {
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(sk);
+            return mac.doFinal(data);
+        } catch (Exception e){
+            throw new TlsException("didnt find SHA256 at provider lul");
+        }
+    }
+
     /**
      * Computes HKDF-Extract output as defined in RFC 5869
      *
@@ -64,7 +78,17 @@ public class HkdFunction {
      * @return The HKDF-Extracted output
      */
     public static byte[] extract(byte[] salt, byte[] ikm) {
-        throw new UnsupportedOperationException("Add code here");
+        //throw new UnsupportedOperationException("Add code here");
+        try {
+            if(salt.length <= 0){
+                salt = new byte[256/8];
+            }
+            return hmacSHA256(salt, ikm);
+
+        }catch (Exception e){
+            throw new TlsException("SHA 256 not found at provider luuuul");
+        }
+
     }
 
     /**
@@ -76,14 +100,42 @@ public class HkdFunction {
      * @return The expanded bytes
      */
     public static byte[] expand(byte[] prk, byte[] info, int outLen) {
-        throw new UnsupportedOperationException("Add code here");
+        //throw new UnsupportedOperationException("Add code here");
+        int N = (int)Math.ceil((double)outLen/32);
+        byte[] t = new byte[0];
+        byte[] okm = new byte[0];
+        int i = 1;
+        while(okm.length < outLen){
+            byte[] input = Util.concatenate(t, info, new byte[]{(byte)i});
+            t = hmacSHA256(prk, input);
+            okm = Util.concatenate(okm, t);
+            i++;
+        }
+        return Arrays.copyOfRange(okm, 0, outLen);
     }
 
     /**
      * Computes the HKDF-Label as defined in TLS 1.3
      */
     private static byte[] labelEncoder(byte[] hashValue, String labelIn, int outLen) {
-        throw new UnsupportedOperationException("Add code here");
+        //throw new UnsupportedOperationException("Add code here");
+        byte[] res = new byte[0];
+        byte[] outLenBytes = Util.convertIntToBytes(outLen, 2);
+
+        byte[] labelLenBytes = Util.convertIntToBytes(("tls13 " + labelIn).length(),1);
+        byte[] tlsLabelBytes = "tls13 ".getBytes(StandardCharsets.UTF_8);
+        byte[] labelInBytes = labelIn.getBytes(StandardCharsets.UTF_8);
+        byte[] hashLenBytes = Util.convertIntToBytes(hashValue.length, 1);
+        return Util.concatenate(outLenBytes, labelLenBytes, tlsLabelBytes, labelInBytes, hashLenBytes, hashValue);
+    }
+
+    private static byte[] sha256(byte[] data){
+        try {
+            MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+            return sha256.digest(data);
+        }catch (Exception e){
+            throw new TlsException("no sha256 found at provider lol");
+        }
     }
 
     /**
@@ -95,7 +147,9 @@ public class HkdFunction {
      * @return the derived secret
      */
     public static byte[] deriveSecret(byte[] prk, String labelIn, byte[] toHash) {
-        throw new UnsupportedOperationException("Add code here");
+        //throw new UnsupportedOperationException("Add code here");
+        byte[] digest = sha256(toHash);
+        return expandLabel(prk, labelIn, digest, 32);
     }
 
     /**
@@ -109,7 +163,9 @@ public class HkdFunction {
      * @return The expanded Label bytes
      */
     public static byte[] expandLabel(byte[] prk, String labelIn, byte[] hashValue, int outLen) {
-        throw new UnsupportedOperationException("Add code here");
+        //throw new UnsupportedOperationException("Add code here");
+        byte[] info = labelEncoder(hashValue,labelIn, outLen);
+        return expand(prk, info, outLen);
     }
 
     private HkdFunction() {
